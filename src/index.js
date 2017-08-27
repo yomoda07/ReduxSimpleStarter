b32f783b0c1d1d0e1981c5b8471c61edd97ad467
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-27 01:33:39 -0700

    Add basic code and api key for youtube search

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReacDOM from 'react-dom'
+
+const API_KEY = 'AIzaSyDQWHReaUbHopDh0-l4lI2rKD0-0ce61u4'
+
+
+const App = () => {
+  return <div>Hi</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('container'));
