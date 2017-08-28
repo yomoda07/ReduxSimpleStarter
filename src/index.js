4982967444463e1b7089583fb95f747c173129d2
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-28 01:12:23 -0700

    Create a component for search bar and include it in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReacDOM from 'react-dom'
-
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyDQWHReaUbHopDh0-l4lI2rKD0-0ce61u4'
 
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.querySelector('container'));
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+);
